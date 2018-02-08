576d456ed0140fd308d7ae01023db1c9b4b84523
trumpetcoder <kevindjames55@gmail.com>
2018-02-08 15:43:45 -0700

    added forms and search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { HttpModule } from '@angular/http'; //added for homework
+
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule, //added for homework
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  results: object []
+  
+  constructor(
+  	public navCtrl: NavController, 
+  	private http: Http
 
-  constructor(public navCtrl: NavController) {
+  	) { }
 
+  ngOnInit() {
   }
 
+  findCharacter(name) {
+		console.log('finding ' + name);
+		this.http.get('https://swapi.co/api/people/?search=' + name)
+		.toPromise()
+		// .then(response => console.log(response.json().results));
+		.then(response => this.results = response.json().results);
+	}
+
 }
+
+
+
+
